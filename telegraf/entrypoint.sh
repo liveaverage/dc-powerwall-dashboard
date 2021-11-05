d93cff2e3e4bb20dfb08350a15597f325ddbf373
J.R. Morgan <liveaverage@gmail.com>
2021-11-05 16:28:42 -0400

    Fix missing var

--- telegraf/entrypoint.sh
+++ telegraf/entrypoint.sh
@@ -91,7 +91,7 @@ while sleep 60; do
     PROCESS_2_STATUS=$?
     # If the greps above find anything, they exit with 0 status
     # If they are not both 0, then something is wrong
-    if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+    if [ $PROCESS_2_STATUS -ne 0 ]; then
         echo "One of the processes has already exited."
         exit 1
     fi
